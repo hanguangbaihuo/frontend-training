ba443c1aec8964375c3199d6a044d1a6ddc27a0a
明华 <shiminghua2008@qq.com>
2021-01-09 21:32:49 +0800

    添加文本组件，加载数据组件，没有数据提示组件，网页加载组件

--- flutter/shop_flutter/lib/widgets/loading_dialog_widget.dart
+++ flutter/shop_flutter/lib/widgets/loading_dialog_widget.dart
@@ -2,7 +2,31 @@
  * @Author: 明华
  * @Date: 2021-01-08 11:33:58
  * @LastEditors: 明华
- * @LastEditTime: 2021-01-08 11:34:07
+ * @LastEditTime: 2021-01-09 21:18:59
  * @Description: 加载中对话框组件
  * @FilePath: /shop_flutter/lib/widgets/loading_dialog_widget.dart
  */
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shop_flutter/config/index.dart';
+
+// 加载数据组件
+class LoadingDialogWidget extends StatefulWidget {
+  @override
+  _LoadingDialogWidgetState createState() => _LoadingDialogWidgetState();
+}
+
+class _LoadingDialogWidgetState extends State<LoadingDialogWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: SpinKitFoldingCube(
+          size: ScreenUtil().setWidth(60.0),
+          color: KColor.watingColor,
+        ),
+      ),
+    );
+  }
+}
--- flutter/shop_flutter/lib/widgets/webview_widget.dart
+++ flutter/shop_flutter/lib/widgets/webview_widget.dart
@@ -2,7 +2,37 @@
  * @Author: 明华
  * @Date: 2021-01-08 11:34:45
  * @LastEditors: 明华
- * @LastEditTime: 2021-01-08 11:34:46
+ * @LastEditTime: 2021-01-09 21:31:34
  * @Description: 网页加载组件
  * @FilePath: /shop_flutter/lib/widgets/webview_widget.dart
  */
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+// 网页加载组件
+class WebViewWidget extends StatelessWidget {
+  // 路径
+  var url;
+  // 标题
+  var title;
+  // 构造方法
+  WebViewWidget(this.url, this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // 显示网页标题
+        title: Text(title),
+        centerTitle: true,
+      ),
+      // 使用 WebViewScaffold 加载网页
+      body: WebviewScaffold(
+        url: url,
+        withZoom: false,
+        withLocalStorage: true,
+        withJavascript: true,
+      ),
+    );
+  }
+}
